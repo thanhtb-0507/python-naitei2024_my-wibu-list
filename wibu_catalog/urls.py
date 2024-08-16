4fddd0f8af871bcf6ae8956a8f00909c3fa8b90a
thanhtb-0507 <tran.ba.thanh-b@sun-asterisk.com>
2024-08-16 16:38:51 +0700

    Task #79875: Tân trang web xem danh sách và xem chi tiết thông tin anime manga
    Added and fix:
      - Anime detail view:
        - Status option dropdown
        - Rate it button
        - Average score display
        - Favorite display
        - Comment section
      - Anime list view:
        - Create related css files
        - Added default picture
        - Redirect to detail when click img or name
        - Pagination
    
    Todo:
      - Manga list and Manga detail view

--- wibu_catalog/urls.py
+++ wibu_catalog/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('register/', views.register, name='register'),
+
     # url mapping for list views
     path('anime/', views.AnimeListView.as_view(), name='anime'),
     path('manga/', views.MangaListView.as_view(), name='manga'),
@@ -13,6 +14,7 @@ urlpatterns = [
     # url mapping for detail views
     path('anime/<int:pk>', views.AnimeDetailView.as_view(), name='anime_detail'),
     path('manga/<int:pk>', views.MangaDetailView.as_view(), name='manga_detail'),
+
     #url mapping for warehouse views
     path('product/',views.list_product,name='product'),
     path('search_content/', views.search_content, name='search_content'),
